da30c597f697f82cd1c6ec6131453f0f0db76695
sqlongithub <nniclasss95@gmail.com>
2024-03-21 20:08:11 +0100

    operations: make operations instantiable without arguments

--- src/main/kotlin/me/sql/infiniteitems/item/action/operation/OperationType.kt
+++ src/main/kotlin/me/sql/infiniteitems/item/action/operation/OperationType.kt
@@ -1,8 +1,14 @@
 package me.sql.infiniteitems.item.action.operation
 
-enum class OperationType {
+import me.sql.infiniteitems.Selectable
+import org.bukkit.Material
+import kotlin.reflect.KClass
 
-    NONE,
-    SEND_MESSAGE,
+enum class OperationType(override val parentClass: KClass<*>,
+                         override val material: Material,
+                         override val description: String) : Selectable {
+
+    NONE(NoneOperation::class, Material.BARRIER, "Do nothing"),
+    SEND_MESSAGE(SendMessageOperation::class, Material.WRITABLE_BOOK, "Send a message"),
 
 }
--- src/main/kotlin/me/sql/infiniteitems/item/action/operation/SendMessageOperation.kt
+++ src/main/kotlin/me/sql/infiniteitems/item/action/operation/SendMessageOperation.kt
@@ -4,18 +4,20 @@ import me.sql.infiniteitems.item.action.operation.data.MessageOperationData
 import me.sql.infiniteitems.item.action.operation.data.OperationData
 import me.sql.infiniteitems.item.action.operation.data.PlayersOperationData
 
-class SendMessageOperation(private val message: MessageOperationData, private val players: PlayersOperationData) : Operation {
+class SendMessageOperation(private val message: MessageOperationData?, private val players: PlayersOperationData?) : Operation {
+
+    constructor() : this(null, null)
 
     override val type = OperationType.SEND_MESSAGE
     override val name: String = "Send Message"
     override val description: String
         get() = "Send a message to §a${getFormattedPlayers()}"
-    override val data: List<OperationData>
+    override val data: List<OperationData?>
         get() = listOf(players)
 
-    fun getFormattedPlayers(): String {
-        val players = players.getPlayers()
-        if(players.isEmpty()) {
+    private fun getFormattedPlayers(): String {
+        val players = players?.getPlayers()
+        if(players.isNullOrEmpty()) {
             return "no one."
         } else {
             var string = ""
@@ -28,6 +30,7 @@ class SendMessageOperation(private val message: MessageOperationData, private va
     }
 
     override fun execute() {
+        if(message == null || players == null) return
         for(player in players.getPlayers()) {
             player.sendMessage(message.getMessage(player))
         }
