d0264d70f13521fdd450ad80b4f6f1d2aee07743
sqlongithub <nniclasss95@gmail.com>
2024-03-21 20:08:58 +0100

    refactor: prettify ActionType

--- src/main/kotlin/me/sql/infiniteitems/item/action/ActionType.kt
+++ src/main/kotlin/me/sql/infiniteitems/item/action/ActionType.kt
@@ -1,12 +1,17 @@
 package me.sql.infiniteitems.item.action
 
+import me.sql.infiniteitems.Selectable
 import me.sql.infiniteitems.item.action.handler.ActionHandler
 import me.sql.infiniteitems.item.action.handler.RightClickActionHandler
 import org.bukkit.Material
 import kotlin.reflect.KClass
 
-enum class ActionType(val actionClass: KClass<out Action>, val handlerClass: KClass<out ActionHandler<Action>>,
-                      val material: Material, val whenever: String, val appliesTo: List<Material> = Material.entries) {
+enum class ActionType(
+    override val parentClass: KClass<*>,
+    val handlerClass: KClass<out ActionHandler<Action>>,
+    override val material: Material,
+    override val description: String,
+    val appliesTo: List<Material> = Material.entries) : Selectable {
 
     // ATTACK_ENTITY(this::class, Material.WOODEN_SWORD, "the player attacks an entity"),
     // ATTACK_BLOCK(Material.WOODEN_PICKAXE, "the player begins breaking a block"),
